d94ad1f9ac74f9bbadd9366c13576b1f8259324f
AiBaeHyunee <1173987538@qq.com>
2020-12-14 19:15:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/terminal2020_shenaijia2018110532/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.terminal2020_shenaijia2018110532.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
